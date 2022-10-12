a49c77a2f1752a8bef91ac4d3e0c99223dd74b22
fhollenbach <florian.hollenbach@gmail.com>
2022-10-12 21:07:23 +0200

    fixed description email

--- EBMAforecast/src/RcppExports.cpp
+++ EBMAforecast/src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // oneMultinomCalt
 IntegerVector oneMultinomCalt(NumericVector probs);
 RcppExport SEXP _EBMAforecast_oneMultinomCalt(SEXP probsSEXP) {
